edaa7db409823901dcdb8d4d959244f4757e80af
tc.11927 <tcalaquian2@my.bcit.ca>
2026-02-02 21:59:15 -0800

    added mini mes

--- src/pages/work/bandit/bandit-breakout.jsx
+++ src/pages/work/bandit/bandit-breakout.jsx
@@ -189,7 +189,10 @@ export default function BanditBreakout() {
                                 />
                             </div>
                         </div>
-
+                        <img
+                            src="/work/minime/examine.svg"
+                            className="floating-overlay1"
+                        />
                         <div className="bandit-text-content bandit-research-text">
                             <h2>Research</h2>
                             <p>
@@ -466,7 +469,10 @@ export default function BanditBreakout() {
                             Unexpected Issue
                         </span>
                     </div>
-
+  <img
+                            src="/work/minime/confusion.svg"
+                            className="floating-overlay2"
+                        />
                     <div className="bandit-two-column">
                         <div className="bandit-media-column">
                             <img
@@ -510,7 +516,10 @@ export default function BanditBreakout() {
                             What Didn't Work & How We Solved it
                         </span>
                     </div>
-
+  <img
+                            src="/work/minime/study.svg"
+                            className="floating-overlay3"
+                        />
                     <div className="bandit-branding-text">
                         <h2>What Didn't Work & How We Solved it</h2>
                         <p>
@@ -624,7 +633,10 @@ export default function BanditBreakout() {
                             title="Bandit Breakout – The Journey"
                         />
                     </div>
-
+<img
+                            src="/work/minime/celebrate.svg"
+                            className="floating-overlay4"
+                        />
                     <div className="bandit-branding-text">
                         <h2>The Journey & Impact</h2>
                         <p>
