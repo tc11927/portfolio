baa662dbf34b31d097db9bef8e8fb45451888724
tc.11927 <tcalaquian2@my.bcit.ca>
2026-02-05 10:52:57 -0800

    general layout complete for about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,8 +4,64 @@ export default function About() {
     return (
         <div>
             <NavBar />
-            <h1 className="about-page1">About Me</h1>
-            <p className="about-page1">Under Construction!</p>
+            <div className="fullaboutsection">
+                <div className="about-row">
+                <div className="about-column">
+                <h1 className="about-me-h1">About Me</h1>
+                <div className="about-me-buttons">
+                    <p className="graphic-design-about">Graphic Design</p>
+                    <p className="UIUX-design-about">UI/UX Design</p>
+                    <p className="motiongraphics-design-about">
+                        Motion Graphics
+                    </p>
+                </div>
+                <div className="about-info">
+                    <div className="about-info-one">
+                        <p>
+                            Hi, I’m Thea, a UX/UI and Graphic Designer from
+                            Vancouver, B.C., who is rooted in creating designs
+                            rooted in curiosity and care. I love approaching
+                            each project where I can bring both joy and bring my
+                            user-first mindset into play.
+                        </p>
+                    </div>
+                    <div className="about-info-two">
+                        <p>
+                            I’ve always had a passion for art, starting with
+                            drawing and animating. Over time, I found myself
+                            enjoying graphic design and technology, as it
+                            allowed me to blend my creativity and desire to make
+                            meaningful products. Now, I strive to design in a
+                            way that sparks curiosity and connection.
+                        </p>
+                    </div>
+                    <div className="about-info-three">
+                        <p>
+                            When I’m not designing, you’ll probably find me
+                            drawing, exploring nature, or listening to music. My
+                            desire to search and create drive my work, and I
+                            believe that in no matter what I do, creativity and
+                            care is key.
+                        </p>
+                    </div>
+                    <div className="about-info-four">
+                        <p>
+                            I’d love to collaborate and connect to create some
+                            amazing experiences!
+                        </p>
+                    </div>
+                </div>
+               </div>
+               <div className="about-column">
+                    <section className="me-photo-section">
+                    <img
+                        src="/about/placeholderimage.png"
+                        className="me-photo-about"
+                    />
+                </section>
+               </div>
+                </div>
+            </div>
         </div>
     );
 }
