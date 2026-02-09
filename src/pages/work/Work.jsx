e08996f72caa457353992ca2db6f8f6982c2242d
tc.11927 <tcalaquian2@my.bcit.ca>
2026-02-08 20:12:50 -0800

    no animated header sad because cloudflare cannot handle

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -58,7 +58,7 @@ export default function Work() {
                     <div className="header-wrapper">
                         <JigglyGrid />
                    <img
-  src="/work/workheader.gif"
+  src="/work/header.png"
   className="header-title"
   alt="Work header"
 
