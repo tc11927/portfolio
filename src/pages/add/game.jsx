7446f8123e36705a429eaa1329e1ba98e0c703ce
tc.11927 <tcalaquian2@my.bcit.ca>
2026-02-24 23:10:45 -0800

    make start and end buttons

--- src/pages/add/game.jsx
+++ src/pages/add/game.jsx
@@ -2,27 +2,33 @@ import { useRef, useEffect, useState } from "react";
 import "./game.css";
 
 export default function CatchingGame() {
-const [score, setScore] = useState(0);
-const [lives, setLives] = useState(3);
-const [basketX, setBasketX] = useState(0);
-const [gameActive, setGameActive] = useState(false);
-const [gameOver, setGameOver] = useState(false);
-const gameAreaRef = useRef(null);
+    const [score, setScore] = useState(0);
+    const [lives, setLives] = useState(3);
+    const [basketX, setBasketX] = useState(0);
+    const [gameActive, setGameActive] = useState(false);
+    const [gameOver, setGameOver] = useState(false);
+    const gameAreaRef = useRef(null);
 
     return (
         <div className="catching-game">
             <div className="game-title">
                 <h2>Catch the Cat!</h2>
-                <p>Catch the cats and save the day</p>
-                <p>Start Game</p>
+                <p>Catch the cats and save the day!</p>
+                {!gameActive ? (
+                    <button
+                        className="start-btn"
+                        onClick={() => setGameActive(true)}>
+                        Start Game
+                    </button>
+                ) : null}
             </div>
             <div className="game-area">
                 <div className="game-upper-area">
                     <div className="game-score">
-                        <p className="score">Score:</p>
+                        <p className="score">Score:{score}</p>
                     </div>
                     <div className="game-lives">
-                        <p className="lives">Lives:</p>
+                        <p className="lives">Lives:{lives}</p>
                     </div>
                 </div>
                 <div className="game-lower-area">
@@ -45,6 +51,17 @@ const gameAreaRef = useRef(null);
                             src=""
                             className="player"></img>
                     </div>
+                    {gameOver && (
+                        <div className="game-over-overlay">
+                            <h3>Game Over! Better Luck Next Time!</h3>
+                            <p>Cats Saved: {score}</p>
+                            <button
+                                className="restart-btn"
+                                onClick={restart}>
+                                Play Again
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
